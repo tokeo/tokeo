18941da2e1461316fd33eaf925e34e04026caf89
Tom Freudenberg <tom.freudenberg@4commerce.de>
2024-02-15 05:19:20 +0100

    mod: use app exported nicegui and restful example

--- tokeo/core/pages/index.py
+++ tokeo/core/pages/index.py
@@ -1,10 +1,17 @@
-from nicegui import ui
+from tokeo.ext.appshare import app
+ui = app.nicegui.ui
+tw = app.nicegui.tw
+
+
+@app.nicegui.fastapi_app.get('/api')
+async def get_api():
+    return { 'msg': 'json api result' }
 
 
 @ui.page('/hello-world')
-def hello_function3():
+def hello_function():
     ui.label('Hello world!').tailwind('text-2xl', 'm-2')
 
 
 def default():
-    ui.label('This is the homepage!').tailwind('text-2xl', 'm-2')
+    tw.h1('text-2xl m-2', text='This is the homepage!')
