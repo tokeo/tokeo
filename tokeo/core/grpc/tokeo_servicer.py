2c8b3408984931728030f690073adb8e44541ebb
Tom Freudenberg <tom.freudenberg@4commerce.de>
2025-01-31 19:01:58 +0100

    fix: align to new print handler

--- tokeo/core/grpc/tokeo_servicer.py
+++ tokeo/core/grpc/tokeo_servicer.py
@@ -1,3 +1,4 @@
+from tokeo.ext.appshare import app
 from tokeo.core import tasks
 from proto import tokeo_pb2_grpc
 from google.protobuf import empty_pb2
@@ -6,6 +7,6 @@ from google.protobuf import empty_pb2
 class TokeoServicer(tokeo_pb2_grpc.TokeoServicer):
 
     def CountWords(self, request, context):
-        print('TokeoServicer CountWords called for: ', request.url)
+        app.print('TokeoServicer CountWords called for: ', request.url)
         tasks.actors.count_words.send(request.url)
         return empty_pb2.Empty()
