bee5017abbe4e84337f0246b24c761778c87b144
Tom Freudenberg <tom.freudenberg@4commerce.de>
2025-02-24 16:32:03 +0100

    add: simple automate configuration and command task

---
+++ tokeo/core/tasks/automate.py
@@ -0,0 +1,6 @@
+from tokeo.core import tasks
+
+
+def uname(app, connection, verbose=False, flags=None):
+    app.log.info('Automation uname called')
+    return connection.run(f'uname {" ".join(flags)}', hide=not verbose, warn=False)
