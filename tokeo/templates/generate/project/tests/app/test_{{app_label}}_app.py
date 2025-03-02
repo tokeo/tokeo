4ac8bbdf02ad2f323144576462f1a526c5e7592c
Tom Freudenberg <tom.freudenberg@4commerce.de>
2025-03-02 11:57:07 +0100

    fix: test class templates

--- tokeo/templates/generate/project/tests/app/test_{{app_label}}_app.py
+++ tokeo/templates/generate/project/tests/app/test_{{app_label}}_app.py
@@ -1,9 +1,27 @@
+from cement.utils.misc import init_defaults
 from {{ app_label }}.main import {{ app_class_name }}Test
+
+
+defaults = init_defaults('')
+
+
+class {{ app_class_name }}TestApp({{ app_class_name }}Test):
+
+    class Meta:
+        # set framework extensions
+        extensions = [
+            'colorlog',
+            'tokeo.ext.yaml',
+            'tokeo.ext.appenv',
+            'tokeo.ext.print',
+            'tokeo.ext.jinja2',
+            'tokeo.ext.appshare',
+        ]
 
 
 def test_{{ app_label }}():
     # test {{ app_label }} without any subcommands or arguments
-    with {{ app_class_name }}Test() as app:
+    with {{ app_class_name }}TestApp(config_defaults=defaults) as app:
         app.run()
         assert app.exit_code == 0
 
@@ -11,6 +29,6 @@ def test_{{ app_label }}():
 def test_{{ app_label }}_debug():
     # test that debug mode is functional
     argv = ['--debug']
-    with {{ app_class_name }}Test(argv=argv) as app:
+    with {{ app_class_name }}TestApp(argv=argv, config_defaults=defaults) as app:
         app.run()
         assert app.debug is True
--- tokeo/templates/generate/project/tests/test_init.py
+++ tokeo/templates/generate/project/tests/test_init.py
@@ -1,5 +1,5 @@
 # ensure we don't break imports from cement namespace
 
 
-def test_import():
+def test_cement_import():
     from cement import App, Controller, ex, init_defaults  # noqa: F401
