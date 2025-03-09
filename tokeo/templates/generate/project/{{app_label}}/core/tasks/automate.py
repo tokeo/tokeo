50d728bb127547e2b99648cc2cb5e93af553f0c3
Tom Freudenberg <tom.freudenberg@4commerce.de>
2025-03-09 01:03:34 +0100

    mod: extended automation example

--- tokeo/templates/generate/project/{{app_label}}/core/tasks/automate.py
+++ tokeo/templates/generate/project/{{app_label}}/core/tasks/automate.py
@@ -1,3 +1,15 @@
+from spiral.core import tasks
+
+
+def count_words(app, connection, verbose=False):
+    app.log.info(f'Automation count_words called')
+    result = connection.run('uname -mrs', hide=not verbose, warn=False)
+    url = f'https://goole.com/q="{result.stdout}"'.replace('\n', '').replace('\r', '')
+    app.log.info(f'Run actor from automation with url: {url}')
+    tasks.actors.count_words.send(url)
+    return True
+
+
 def uname(app, connection, verbose=False, flags=None):
     app.log.info('Automation uname called')
     return connection.run(f'uname {" ".join(flags)}', hide=not verbose, warn=False)
