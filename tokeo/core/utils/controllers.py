dfa999ad1c57afe3b5aedc22df456e20a3ced721
Tom Freudenberg <tom.freudenberg@4commerce.de>
2025-03-17 15:17:11 +0100

    mod: rewrite the docstring in valid working md comment

--- tokeo/core/utils/controllers.py
+++ tokeo/core/utils/controllers.py
@@ -1,28 +1,49 @@
 def subcmdmeta(controller):
     """
-    Access the __cement_meta__ information of subcommand within the command.
+    Access the \\_\\_cement_meta\\_\\_ information of subcommand within the command.
 
-    Args:
-        controller: The controller instance
+    ### Args:
 
-    Returns:
-        The __cement_meta__ information of the subcommand
+    - **controller** (Controller): The controller instance
+
+    ### Returns:
+
+    - **\\_\\_cement_meta\\_\\_** (Meta): object ref to Meta information of the
+      subcommand
+
     """
     return getattr(controller, controller.app.pargs.__dispatch__.split('.')[1]).__cement_meta__
 
 
 def defaultmeta(controller):
     """
-    Access the __cement_meta__ information of default subcommand within the command.
+    Access the \\_\\_cement_meta\\_\\_ information of default subcommand within the
+    command.
 
-    Args:
-        controller: The controller instance
+    ### Args:
 
-    Returns:
-        The __cement_meta__ information of the default subcommand
+    - **controller** (Controller): The controller instance
+
+    ### Returns:
+
+    - **\\_\\_cement_meta\\_\\_** (Meta): object ref to Meta information of the
+      _default_ subcommand
+
     """
     return getattr(controller, controller._meta.default_func).__cement_meta__
 
 
 def controller_log_info_help(controller):
+    """
+    Log the help (dry) from subcmd's \\_\\_cement_meta\\_\\_.
+
+    ### Args:
+
+    - **controller** (Controller): The controller instance
+
+    ### Output:
+
+    :  Log the controller's `Meta`.`help` to the application info logger
+
+    """
     controller.app.log.info(subcmdmeta(controller).parser_options['help'])
