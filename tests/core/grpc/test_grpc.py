0803ad0a46f78a7a60de75ac0cb09ad4e344de9d
Tom Freudenberg <tom.freudenberg@4commerce.de>
2026-06-12 23:20:37 +0200

    fix: tests aligned to extension rules

--- tests/core/grpc/test_grpc.py
+++
@@ -1,9 +0,0 @@
-from tokeo.main import TokeoTest
-
-
-def test_grpc_count_words():
-    # test command1 with arguments
-    argv = ['grpc', 'count-words', '--url', 'https://github.com']
-    with TokeoTest(argv=argv) as app:
-        app.run()
-        assert app.last_rendered is None
